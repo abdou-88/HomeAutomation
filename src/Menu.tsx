7c05657d6f3f0f6295b664699d1ea43dc3883717
ABDELLAH YOUSSFI <abdellah.y.ouahabi@gmail.com>
2023-11-20 13:54:46 +0000

    menu done aimation

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -4,13 +4,13 @@ import './App.css'
 import './assets/Menu.css'
 
 import { Icon } from '@mdi/react';
-import { mdiHomeAutomation, mdiMusic, mdiMovie } from '@mdi/js';
+import { mdiCurtains, mdiMusic, mdiMovie, mdiLightbulb, mdiHomeAutomation} from '@mdi/js';
 
 function Menu() {
-    const [isFlipped, setIsFlipped] = useState([false, false, false]);
+    const [isFlipped, setIsFlipped] = useState([false, false, false, false, false,false]);
 
     const handleClick = (id: number ) => {
-        const newArray = isFlipped.map((value, i) => (i == id ? !isFlipped[id] : false));
+        const newArray = isFlipped.map((_value, i) => (i == id ? !isFlipped[id] : false));
         setIsFlipped(newArray);
     };
     return (
@@ -19,28 +19,40 @@ function Menu() {
                 <div className={`flip-container ${isFlipped[1] ? 'flip' : ''}`} onClick={() => handleClick(1)}>
                     <div className="flipper">
                         <div className="front">
-                            <Icon path={mdiHomeAutomation}
+                            <Icon path={mdiLightbulb}
                             size={4}
                             horizontal
                             vertical
                             rotate={180}
                             color="white" /></div>
-                        <div className="back">Home Control</div>
+                        <div className="back">Lights</div>
                     </div>
                 </div>
                 <div className={`flip-container ${isFlipped[2] ? 'flip' : ''}`} onClick={() => handleClick(2)}>
                     <div className="flipper">
                         <div className="front">
-                            <Icon path={mdiMusic}
+                            <Icon path={mdiCurtains}
                             size={4}
                             horizontal
                             vertical
                             rotate={180}
                             color="white" /></div>
-                        <div className="back">Music</div>
+                        <div className="back">Curtains</div>
                     </div>
                 </div>
                 <div className={`flip-container ${isFlipped[3] ? 'flip' : ''}`} onClick={() => handleClick(3)}>
+                    <div className="flipper">
+                        <div className="front">
+                            <Icon path={mdiMusic}
+                                size={4}
+                                horizontal
+                                vertical
+                                rotate={180}
+                                color="white" /></div>
+                        <div className="back">Music</div>
+                    </div>
+                </div>
+                <div className={`flip-container ${isFlipped[4] ? 'flip' : ''}`} onClick={() => handleClick(4)}>
                     <div className="flipper">
                         <div className="front">
                             <Icon path={mdiMovie}
@@ -52,6 +64,19 @@ function Menu() {
                         <div className="back">Movies</div>
                     </div>
                 </div>
+                <div className={`flip-container ${isFlipped[5] ? 'flip' : ''}`} onClick={() => handleClick(5)}>
+                    <div className="flipper">
+                        <div className="front">
+                            <Icon path={mdiHomeAutomation}
+                                size={4}
+                                horizontal
+                                vertical
+                                rotate={180}
+                                color="white" /></div>
+                        <div className="back">Movies</div>
+                    </div>
+                </div>
+                
             </div>
         </>
     );
