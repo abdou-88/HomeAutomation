ff4080b48b2d788f4fcdc3ad7e43ffd9d6253245
ABDELLAH YOUSSFI <abdellah.y.ouahabi@gmail.com>
2023-11-24 16:16:53 +0000

    music player

--- src/Components/Music/Music.tsx
+++ src/Components/Music/Music.tsx
@@ -1,20 +1,120 @@
 
+import React, { useEffect, useState } from 'react';
+import { Container, IconButton, Slider } from '@mui/material';
+import { PlayArrow, Pause, SkipNext, SkipPrevious } from '@mui/icons-material';
+import { Howl } from 'howler';
+
 
 interface MusicProps {
     content?: React.ReactNode | null;
     isOpen?: boolean;
 }
+interface Track {
+    title: string;
+    audioSrc: string;
+}
+
+
+const tracks: Track[] = [
+    {
+        title: 'Track 1',
+        audioSrc: 'C:/Users/abdou/Desktop/VS Projects/HomeAutomation/public/T1.mp3',
+    },
+    {
+        title: 'Track 2',
+        audioSrc: 'C:/Users/abdou/Desktop/VS Projects/HomeAutomation/public/T2.mp3',
+    },
+    {
+        title: 'Track 3',
+        audioSrc: 'C:/Users/abdou/Desktop/VS Projects/HomeAutomation/public/T3.mp3',
+    },
+    // Add more tracks as needed
+];
+
 
 const Music: React.FC<MusicProps> = () => {
 
+    const [currentTrackIndex, setCurrentTrackIndex] = useState<number>(0);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [audio, setAudio] = useState<Howl | null>(null);
+
+
+    useEffect(() => {
+        setAudio(
+            new Howl({
+                src: [tracks[currentTrackIndex].audioSrc],
+                onend: () => handleNextTrack(),
+                onloaderror: (id, error) => {
+                    console.error('Error loading sound:', error);
+                },
+            })
+        );
+    }, [currentTrackIndex]);
+
+    const handlePlayPause = () => {
+        if (audio) {
+            if (isPlaying) {
+                audio.pause();
+            } else {
+                audio.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    const handleNextTrack = () => {
+        if (currentTrackIndex < tracks.length - 1) {
+            setCurrentTrackIndex(currentTrackIndex + 1);
+            setIsPlaying(false);
+        }
+    };
+
+    const handlePrevTrack = () => {
+        if (currentTrackIndex > 0) {
+            setCurrentTrackIndex(currentTrackIndex - 1);
+            setIsPlaying(false);
+        }
+    };
+
+
+    const handleSliderChange = (_event: Event, newValue: number | number[]) => {
+        if (audio) {
+        const seekTime = typeof newValue === 'number' ? newValue : newValue[0];
+        audio.seek(seekTime);}
+    };
 
 
 
     return (
-        <>
-            <div>Music</div>
-        </>
+        <Container>
+
+
+            <h1>{tracks[currentTrackIndex].title}</h1>
+            <div>
+                <IconButton onClick={handlePrevTrack}>
+                    <SkipPrevious />
+                </IconButton>
+                <IconButton onClick={handlePlayPause}>
+                    {isPlaying ? <Pause /> : <PlayArrow />}
+                </IconButton>
+                <IconButton onClick={handleNextTrack}>
+                    <SkipNext />
+                </IconButton>
+            </div>
+         
+            <Slider
+                value={audio.seek()}
+                min={0}
+                max={audio.duration()}
+                onChange={handleSliderChange}
+                aria-labelledby="continuous-slider"
+            />
+        </Container>
     );
+
 }
 
 export default Music
+
+
+
